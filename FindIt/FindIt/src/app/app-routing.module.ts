497d0cf8c591e0bb01f1fc2eaab6614ba777c8f3
fernanman1207 <fe.gomezv@duocuc.cl>
2024-11-24 16:39:03 -0300

    rellenando mas rapido de lo esperado

--- FindIt/FindIt/src/app/app-routing.module.ts
+++ FindIt/FindIt/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { authGuard } from './guards/auth.guard';
+import { authGuard } from './guards/guards/auth.guard';
 
 const routes: Routes = [
   {
