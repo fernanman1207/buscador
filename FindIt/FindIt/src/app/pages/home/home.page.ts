5bf658582fe0aeaa042ad99c92a5803eb434a614
fernanman1207 <fe.gomezv@duocuc.cl>
2024-11-24 16:55:33 -0300

    home listo

--- FindIt/FindIt/src/app/pages/home/home.page.ts
+++ FindIt/FindIt/src/app/pages/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  recentProducts: Product[] = [];
 
+  constructor(private navCtrl: NavController, private productSrv: ProductService) { }
+
+  ngOnInit() {
+    this.loadproducts();
+  }
+
+  loadproducts(){
+    this.productSrv.getProducts().subscribe((products) => {
+      this.recentProducts = products;
+    });
+  }
+
+  navigateToReport() {
+    this.navCtrl.navigateForward('/report');
+  }
+
+  navigateToFind() {
+    this.navCtrl.navigateForward('/find');
+  }
 }
