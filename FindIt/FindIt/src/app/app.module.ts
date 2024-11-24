3158aa4c6ae083039ae83362f718a3568353739e
fernanman1207 <fe.gomezv@duocuc.cl>
2024-11-24 16:10:17 -0300

    empezando con el app component

--- FindIt/FindIt/src/app/app.module.ts
+++ FindIt/FindIt/src/app/app.module.ts
@@ -7,10 +7,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { environment } from 'src/environments/environment.prod';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Inicializar Firebase con la configuración
+    AngularFireAuthModule, // Importar el módulo de autenticación
+    AngularFireDatabaseModule, // Importar el módulo de base de datos
+    AngularFireStorageModule,
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy, }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
