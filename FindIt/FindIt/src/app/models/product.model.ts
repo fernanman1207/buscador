455e9335ccb79670ac0a83bc417aee152b712f9c
fernanman1207 <fe.gomezv@duocuc.cl>
2024-11-24 16:25:41 -0300

    avanzando con modelos

---
+++ FindIt/FindIt/src/app/models/product.model.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id?: string;
+    title: string;
+    description: string;
+    imageUrl: string;
+    status: 'Reportado' | 'Encontrado'  | 'Validado' | 'Entregado';
+    location?: string;
+    dateReported: Date;
+  }
+  
--- FindIt/FindIt/src/app/services/openai.service.ts
+++ FindIt/FindIt/src/app/services/openai.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { OpenAI } from 'openai';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpenaiService {
+  private openai: OpenAI;
+  private apiKey : string = '';
 
-  constructor() { }
-}
+  constructor() {
+    this.openai = new OpenAI({
+      apiKey: this.apiKey, // Reemplaza con tu clave API
+      dangerouslyAllowBrowser: true, // Habilita el uso en navegador
+    });
+  }
+
+    async getRecipe(ingredients: string): Promise<string> {
+      const prompt = `Crea recetas usando estos ingredientes: ${ingredients}`;
+
+      try {
+        const completion = await this.openai.chat.completions.create({
+            model: "gpt-4o",
+            messages: [
+                { role: "system", content: "Eres un experto chef profesional." },
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+        });
+
+        console.log(completion.choices[0].message);
+        //const content = response.choices[0].message.content;
+        let content = completion.choices[0].message.content;
+
+        if (content) {
+          return content; // Retorna las opciones de recetas
+        } else {
+          throw new Error('No se obtuvo contenido de la respuesta.');
+        }
+      } catch (error) {
+        console.error('Error al obtener la receta:', error);
+        throw error;
+      }
+    }
+  }
