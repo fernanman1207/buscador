083765ff3226c07f7e745ff5858eaff4b2cb941c
fernanman1207 <fe.gomezv@duocuc.cl>
2024-11-24 16:50:56 -0300

    faltan las paginas

--- FindIt/FindIt/src/app/components/header-custom/header-custom.component.ts
+++ FindIt/FindIt/src/app/components/header-custom/header-custom.component.ts
@@ -1,14 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-header-custom',
   templateUrl: './header-custom.component.html',
   styleUrls: ['./header-custom.component.scss'],
 })
-export class HeaderCustomComponent  implements OnInit {
+export class HeaderCustomComponent  {
+  @Input() title: string = 'Default Title';  // Título dinámico del encabezado
+  @Input() showBackButton: boolean = true;   // Mostrar u ocultar el botón de volver
+  @Input() showProfileButton: boolean = true; // Mostrar u ocultar el botón de perfil
+  @Input() profileButtonUrl: string = '/profile'; // URL para el botón de perfil
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {}
+
+  goBack() {
+    this.navCtrl.back();
+  }
 
 }
