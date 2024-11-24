5c1155eb2b64aab7810aae118e9f9b51b3e36e12
fernanman1207 <fe.gomezv@duocuc.cl>
2024-11-24 17:06:38 -0300

    listo para mañana

--- FindIt/FindIt/src/app/pages/openai/openai.page.ts
+++ FindIt/FindIt/src/app/pages/openai/openai.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OpenaiService } from 'src/app/services/openai.service';
+
 
 @Component({
   selector: 'app-openai',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./openai.page.scss'],
 })
 export class OpenaiPage implements OnInit {
+  ingredients: string = '';
+  recipes: string = '';
 
-  constructor() { }
+  constructor(private openAiService: OpenaiService) {}
 
   ngOnInit() {
+  }
+
+  async getRecipes() {
+    try {
+      this.recipes = await this.openAiService.getRecipe(this.ingredients);
+    } catch (error) {
+      console.error('Error al obtener recetas:', error);
+    }
   }
 
 }
