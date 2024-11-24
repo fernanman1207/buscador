130da7b4608d3246377a6634e64c7366f6f9f24c
fernanman1207 <fe.gomezv@duocuc.cl>
2024-11-24 16:45:16 -0300

    mas avance

--- FindIt/FindIt/src/app/components/product-card/product-card.component.ts
+++ FindIt/FindIt/src/app/components/product-card/product-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-product-card',
@@ -6,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent  implements OnInit {
+  @Input() product?: Product; // Recibe un objeto Product como entrada
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
-  ngOnInit() {}
+  getStatusColor(status?: string): string {
+    switch (status) {
+      case "Reportado":
+        return 'warning';
+      case "Encontrado":
+        return 'success';
+      case "Validado":
+        return 'tertiary';
+      case "Entregado":
+        return 'medium';
+      default:
+        return 'primary';
+    }
+  }
+
+  ngOnInit() {
+
+  }
+
+  viewProductDetails(event: Event) {
+    event.stopPropagation();  // Evita que el evento de clic se propague al ion-card
+    if (this.product?.id) {
+      // Navegar a la página de detalles del producto
+      this.navCtrl.navigateForward(`/item/${this.product.id}`);
+    }
+  }
 
 }
--- FindIt/FindIt/src/app/components/shared/shared.module.ts
+++ FindIt/FindIt/src/app/components/shared/shared.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
-
+import { ProductCardComponent } from '../product-card/product-card.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { ImageUploaderComponent } from '../image-uploader/image-uploader.component';
+import { HeaderCustomComponent } from '../header-custom/header-custom.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductCardComponent,
+    SearchBarComponent,
+    ImageUploaderComponent,
+    HeaderCustomComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule,
+    RouterModule
+  ],
+  exports: [
+    ProductCardComponent,
+    SearchBarComponent,
+    ImageUploaderComponent,
+    HeaderCustomComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule {}
