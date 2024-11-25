305fae3c25413ba2915ccde426f592f3b1be96cb
fernanman1207 <fe.gomezv@duocuc.cl>
2024-11-25 18:54:08 -0300

    avanzando muy rapido

---
+++ FindIt/android/app/src/main/java/com/fernandoman1207/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fernandoman1207.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ FindIt/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fernandoman1207.app',
+  appName: 'buscacosas',
+  webDir: 'www'
+};
+
+export default config;
