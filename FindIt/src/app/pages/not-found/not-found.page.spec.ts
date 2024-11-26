53e1c1b4a9be4dcb8ca2f33013486ab1b9e6f578
fernanman1207 <fe.gomezv@duocuc.cl>
2024-11-26 09:47:02 -0300

    Update not-found.page.spec.ts

--- FindIt/src/app/pages/not-found/not-found.page.spec.ts
+++ FindIt/src/app/pages/not-found/not-found.page.spec.ts
@@ -1,17 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule, NavController } from '@ionic/angular';
 import { NotFoundPage } from './not-found.page';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('NotFoundPage', () => {
   let component: NotFoundPage;
   let fixture: ComponentFixture<NotFoundPage>;
+  let navCtrlSpy: jasmine.SpyObj<NavController>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    const navSpy = jasmine.createSpyObj('NavController', ['navigateForward']);
+
+    await TestBed.configureTestingModule({
+      declarations: [NotFoundPage],
+      imports: [IonicModule.forRoot(), RouterTestingModule],
+      providers: [{ provide: NavController, useValue: navSpy }],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NotFoundPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    navCtrlSpy = TestBed.inject(NavController) as jasmine.SpyObj<NavController>;
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to "/home" when goHome is called', () => {
+    component.goHome();
+    expect(navCtrlSpy.navigateForward).toHaveBeenCalledWith('/home');
   });
 });
