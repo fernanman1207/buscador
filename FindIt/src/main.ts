0be98d2eae9eea2f9614dbca7d978e49cadc300f
fernanman1207 <fe.gomezv@duocuc.cl>
2024-11-26 10:25:06 -0300

    Update main.ts

--- FindIt/src/main.ts
+++ FindIt/src/main.ts
@@ -1,6 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { registerPlugin } from '@capacitor/core';
+import { Capacitor } from '@capacitor/core';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+  if (Capacitor.isNativePlatform()) {
+    console.log('Running on native platform');
+  }
